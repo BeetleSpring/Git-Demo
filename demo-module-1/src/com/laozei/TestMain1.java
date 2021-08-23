d73313031864dcc32ab9f641cc331bd38c03f755
BeetleSpring <1637413223@qq.com>
2021-08-23 19:43:24 +0800

    测试提交1

--- demo-module-1/src/com/laozei/TestMain1.java
+++ demo-module-1/src/com/laozei/TestMain1.java
@@ -4,5 +4,6 @@ public class TestMain1 {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("ccc");
+        System.out.println("test1");
     }
 }
